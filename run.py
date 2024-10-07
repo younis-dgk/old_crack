a4368b3c4bcf03c3965ce3cff2c0544be125e239
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-07 20:58:43 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,23 @@
-import old
-XYZ()
+#coding=utf-8
+import os, sys, platform
+ 
+os.system('rm -rf old.so old.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf old.so old.so')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('old.so'):
+        os.system('curl -L https://github.com/younis-dgk/old_crack/blob/main/old.cpython-311.so?raw=true -o old.so') 
+        import old
+    else:
+        import old
+ 
+elif bit == '32bit':
+    exit()
+ 
