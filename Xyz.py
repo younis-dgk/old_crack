1bf5561e11901a51bbc06b214871e50ff32d51b5
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-07 17:06:34 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,11 +1,1 @@
-import os,re,time,sys,platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from old import main
-    main()
-elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
- 
- 
- 
+import old
