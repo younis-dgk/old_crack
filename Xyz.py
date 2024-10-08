762078c86daa676dc55b4af59dd025d5090297e0
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-08 23:11:32 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -8,16 +8,7 @@ try:
         os.system('rm -rf old.so')
 except:
     pass
- 
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('old.so'):
-        os.system('curl -L https://github.com/younis-dgk/old_crack/blob/main/old.cpython-311.so?raw=true -o old.so') 
-        import old
-    else:
-        import old
- 
-elif bit == '32bit':
-    exit()
+
+os.system("chmod 777 old")
+os.system("./old")
  
