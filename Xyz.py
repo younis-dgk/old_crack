259d5e2e9de7b99ddae2b0d1e1716770cff7df9e
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 14:59:50 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,16 +1,26 @@
-import os,re,sys,platform
-
-os.system('rm -rf old.so')
+#coding=utf-8
+import os, sys, platform
+os.system("git pull")
+os.system('rm -rf old64.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf old.so')
+        os.system('rm -rf old64.so')
 except:
     pass
  
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import old64
+    if not os.path.isfile('old64.so'):
+        os.system('curl -L https://github.com/younis-dgk/old_crack/blob/main/old64.cpython-312.so?raw=true -o old64.so') 
+        import old64
+    else:
+        import old64
+ 
 elif bit == '32bit':
-    print(f"\033[1;91m Sorry Baby 32 Bit Not Supported .... ");exit() 
+    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
+ 
+ 
+ 
  
