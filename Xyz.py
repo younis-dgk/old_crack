d467fd91d5d797dfe876ff00b74bf4f2262c2e99
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-14 11:56:38 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -5,8 +5,8 @@ os.system("clear")
 print('\033[0;97m [💸] \033[92mChecking For Updates...')
 os.system('git pull --quiet 2>/dev/null')
 x="Script is On Update" 
-y="Please Wait For Next Update")
-z="Authorized by : Muhammad Younis")
+y="Please Wait For Next Update"
+z="Authorized by : Muhammad Younis"
 input(f"\n\033[1;92m Press Enter For join our WhatsApp Group ")
 os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
 print(x) 
