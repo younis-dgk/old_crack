5178c0cd53aca0c0e385911ec1ca77c78dd584cb
GANGSTER <167035490+younis-dgk@users.noreply.github.com>
2024-12-19 03:16:13 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,7 +1,26 @@
-import os
-os.system("clear")
-x='\033[0;97mTool has been deleted permanently\nYounis john no mores updates on it'
-y='For More About Author : https://bio.link/younis_dgk'
-print(x)
-print(y)
-exit()
+#coding=utf-8
+import os, sys, time, platform
+os.system('clear') 
+print('\033[0m [💸] \033[92m join Our WhatsApp group For More Updates 🥰✨') 
+os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
+print('\n\033[0m [✓] \033[92m Checking For Updates .... ') 
+os.system('rm -rf old.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf old.so')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('old.so'):
+        os.system('curl -L https://github.com/younis-dgk/executables/blob/main/old.cpython-312.so?raw=true -o old.so') 
+        import old
+    else:
+        import old
+ 
+elif bit == '32bit':
+    print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
+ 
